0bcbd5662911608aa47080e9713cddd64ef1f8e0
carricom <matthieu.carrico@etu.u-pec.fr>
2019-12-03 15:24:01 +0000

    movOK

--- Matthieu/main.c
+++ Matthieu/main.c
@@ -3,9 +3,10 @@
 #include<graph.h>
 #define ligneMax 40
 #define colonneMax 60
+#define CYCLE 100000L
 
-	int coactux = (colonneMax/2),
-	coactuy = (ligneMax/2);
+int coactux = (colonneMax/2),
+coactuy = (ligneMax/2);
 
 
 int main()
@@ -13,69 +14,99 @@ int main()
 	int map[ligneMax][colonneMax];
 
 	int buttonP = XK_Right;
+	
+	long suivant= Microsecondes()+CYCLE;
 
 
-
-    InitialiserGraphique();
-    CreerFenetre(10,10,colonneMax*10+20,ligneMax*10+80);
-    EffacerEcran(CouleurParComposante(0, 0, 0));
-    ChoisirCouleurDessin(CouleurParNom("green"));
+	InitialiserGraphique();
+	CreerFenetre(10,10,colonneMax*10+20,ligneMax*10+80);
+	EffacerEcran(CouleurParComposante(0, 0, 0));
+	ChoisirCouleurDessin(CouleurParNom("green"));
     RemplirRectangle(10,10,colonneMax*10,ligneMax*10);					/*RemplirRectangle(int x,int y,int l,int h);*/
-    initSnake();
+	initSnake();
 
-   // buttonP = movSnake(buttonP);
-    Touche();
-    
+	for (;;)
+	{
+			if (ToucheEnAttente())
+			{
+				buttonP = checkMovSnake(buttonP);
+			}
 
-    return EXIT_SUCCESS;
+		if (Microsecondes()>suivant)
+		{	
+
+
+			movSnake(buttonP);
+			actuFenetre();
+
+			suivant= Microsecondes()+CYCLE;
+		}
+	}
+
+
+
+	Touche();
+
+
+	return EXIT_SUCCESS;
 }
 
 void initSnake(){
-ChoisirCouleurDessin(CouleurParNom("black"));
-RemplirRectangle(coactux*10,coactuy*10,10,10);
+	ChoisirCouleurDessin(CouleurParNom("black"));
+	RemplirRectangle(coactux*10,coactuy*10,10,10);
 
 }
 
 
-int movSnake(int buttonP){
+int checkMovSnake(int buttonP){
 	if(ToucheEnAttente()){
-		if (Touche() == XK_Left)
-		{
-			buttonP = XK_Left;
+		switch(Touche()){
+			case XK_Left :
+			buttonP = XK_Left;break;
+			case XK_Right :
+			buttonP = XK_Right;break;
+			case XK_Up :
+			buttonP = XK_Up;break;
+			case XK_Down :
+			buttonP = XK_Down;break;
 		}
-		if (Touche() == XK_Right)
-		{
-			buttonP = XK_Right;
-
-		}
-		if (Touche() == XK_Up)
-		{
-			buttonP = XK_Up;
-
-		}
-		if (Touche() == XK_Down)
-		{
-			buttonP = XK_Down;
-
-		}
+		
 	}
 
+
+	return buttonP;
+}
+
+void movSnake(int buttonP){
+
+
 	if (buttonP == XK_Left)
-	{
-		
+	{	
+		coactux--;
+
 	}
 	if (buttonP == XK_Right)
 	{
-
+		coactux++;
 	}
 	if (buttonP == XK_Up)
 	{
-
+		coactuy--;
 	}
 	if (buttonP == XK_Down)
 	{
-		
+		coactuy++;		
 	}
-	return buttonP;
+}
+
+
+
+void actuFenetre(){
+
+	ChoisirCouleurDessin(CouleurParNom("green"));
+	RemplirRectangle(10,10,colonneMax*10,ligneMax*10);
+	ChoisirCouleurDessin(CouleurParNom("black"));
+	RemplirRectangle(coactux*10,coactuy*10,10,10);		
+
 }
 
