c656975df4cdd4bf3b27039a38b292e0937e28fd
Minguet <paul.minguet@etu.u-pec.fr>
2019-12-12 13:51:32 +0100

    ok

--- Matthieu/main.c
+++ Matthieu/main.c
@@ -7,52 +7,13 @@
 
 int coactux = (colonneMax/2),
 coactuy = (ligneMax/2);
+int fin = 0;
 
-/*------------------------------------MAIN------------------------------------*/
-int main()
-{	couleur fond;
-	int map[ligneMax][colonneMax];
-
-	int buttonP = XK_Right;
-	
-	long suivant= Microsecondes()+CYCLE;
-
-
-	InitialiserGraphique();
-
-	CreerFenetre(10,10,colonneMax*10+20,ligneMax*10+80); 		/*creation fenetre*/
-	EffacerEcran(CouleurParComposante(0, 0, 0));				/*coloration du fond en noir*/						
-	ChoisirCouleurDessin(CouleurParNom("green"));				/*selection de la couleur*/
-    RemplirRectangle(10,10,colonneMax*10,ligneMax*10);			/*RemplirRectangle(int x,int y,int l,int h);*/
-	initSnake();
-
-	for (;;)													/*début du jeu*/
-	{
-			if (ToucheEnAttente())								/*verifie si il y a une touche dans le tampon*/
-			{
-				buttonP = checkMovSnake(buttonP);				/*atribue a buttonP la derniere touche préssé*/
-			}
-
-		if (Microsecondes()>suivant)
-		{
-			movSnake(buttonP);
-			actuFenetre();
-
-			suivant= Microsecondes()+CYCLE;
-		}
-	}
-
-
-
-	Touche();
-
-
-	return EXIT_SUCCESS;
-}
 /*------------------------------------initSnake------------------------------------*/
 
 void initSnake(){
 	ChoisirCouleurDessin(CouleurParNom("black"));
+	system("/home/paul/IUT/PT11_APL2019/Paul/serpent.c");
 	RemplirRectangle(coactux*10,coactuy*10,10,10);				/*place le snake au centre afin de commencer */
 
 }
@@ -105,10 +66,66 @@ void movSnake(int buttonP){
 /*------------------------------------actuFenetre------------------------------------*/
 void actuFenetre(){
 
-	ChoisirCouleurDessin(CouleurParNom("green"));
+	ChoisirCouleurDessin(CouleurParNom("grey"));
 	RemplirRectangle(10,10,colonneMax*10,ligneMax*10);
+	ChoisirCouleurDessin(CouleurParComposante(107, 142, 35));
+	RemplirRectangle(20,20,colonneMax*10-20,ligneMax*10-20);
 	ChoisirCouleurDessin(CouleurParNom("black"));
 	RemplirRectangle(coactux*10,coactuy*10,10,10);		
 
+
 }
 
+/*------------------------------------Collisions Mur----------------------------------*/
+int collisionsmurs(){
+	if(coactux*10 <= 10 || coactux*10 >= colonneMax*10 || coactuy <= 1 || coactuy >= ligneMax){
+		fin = 1;
+		EcrireTexte((colonneMax*10+20)/2-TailleChaineEcran("Game Over", 2)/2, (ligneMax*10+80)/2, "Game Over", 2);
+		EcrireTexte((colonneMax*10+20)/2-TailleChaineEcran("Appuyez sur une touche pour quitter", 1)/2, (ligneMax*10+80)/2+20, "Appuyez sur une touche pour quitter", 1);
+	}
+}
+
+/*------------------------------------MAIN------------------------------------*/
+int main()
+{	couleur fond;
+	int map[ligneMax][colonneMax];
+
+	int buttonP = XK_Right;
+	
+	long suivant= Microsecondes()+CYCLE;
+
+
+	InitialiserGraphique();
+
+	CreerFenetre(10,10,colonneMax*10+20,ligneMax*10+80); 		/*creation fenetre*/
+	EffacerEcran(CouleurParComposante(0, 0, 0));				/*coloration du fond en noir*/						
+	ChoisirCouleurDessin(CouleurParComposante(107, 142, 35));				/*selection de la couleur*/
+    RemplirRectangle(10,10,colonneMax*10,ligneMax*10);			/*RemplirRectangle(int x,int y,int l,int h);*/
+	initSnake();
+
+	for (;fin != 1;)													/*début du jeu*/
+	{
+			if (ToucheEnAttente())								/*verifie si il y a une touche dans le tampon*/
+			{
+				buttonP = checkMovSnake(buttonP);				/*atribue a buttonP la derniere touche préssé*/
+			}
+
+		if (Microsecondes()>suivant)
+		{
+			movSnake(buttonP);
+			actuFenetre();
+
+			suivant= Microsecondes()+CYCLE;
+		}
+		collisionsmurs();
+	}
+
+
+
+	Touche();
+
+
+	return EXIT_SUCCESS;
+}
+
+//gcc main.c -I/home/paul/IUT/PT/bibliotheque-graphique-iut-1.1/src/include -L/home/paul/IUT/PT/bibliotheque-graphique-iut-1.1/src/lib -lgraph
